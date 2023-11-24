9d688e873540fe5c5e41e276edb922af746979bc
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-11-24 23:32:51 +0700

    dev: gaussian nb predict proba

--- lib/naive_bayes.py
+++ lib/naive_bayes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class NaiveBayesClassifier():
     def __init__(self, alpha: int) -> None:
@@ -14,29 +15,42 @@ class NaiveBayesClassifier():
         self.y_train = y_train
         self.numerical_columns = numerical_columns
         self.categorical_columns = categorical_columns
+        self.CategoricalProbability()
 
-    def predict(self, x_test):
-        pass
+    def predictProbaNumerical(self, x_test_numerical: list[list[float]]):
+        res = []
+        for x_test_row in x_test_numerical:
+            numerical_probabilities = self.GaussianProbability(x_test_row)
+            res.append(numerical_probabilities)
+        return res
 
-    def GaussianProbability(self, numerical_train: list[list[float]], y_train: list[int], x_numerical_test: list[float]):
-        y_train = np.array(y_train)
-        target_values = np.unique(y_train)
-        log_numerical_probabilities = []
+    def GaussianProbability(self, x_numerical_test: list[float]):
+        y_train_np = np.array(self.y_train)
+        target_values = np.unique(y_train_np)
+        # log_numerical_probabilities = []
+        target_probabilities = []
         for target_value in target_values:
-            log_target_probabilities = []
-            for col, x_train_values in enumerate(numerical_train):
-                indices = np.where(y_train == target_value)
+            # log_target_probabilities = []
+            target_given_all_probabilities = self.target_probs[target_value]
+            for col, x_train_values in enumerate(self.numerical_columns):
+                indices = np.where(y_train_np == target_value)
                 x_train_values = np.array(x_train_values)[indices]
 
                 var = np.var(x_train_values)
                 avg = np.average(x_train_values)
                 xi = x_numerical_test[col]
 
-                log_target_given_x_probability = (-0.5 * np.log(2 * np.pi * var)) - (0.5 * (((xi - avg) ** 2) / var))
-                log_target_probabilities.append(log_target_given_x_probability)
-            log_numerical_probabilities.append(log_target_probabilities)
+                # log_target_given_x_probability = (-0.5 * np.log(2 * np.pi * var)) - (0.5 * (((xi - avg) ** 2) / var))
+                x_given_target_probability = (1 / math.sqrt(2 * np.pi * var)) * math.exp(- ((xi - avg) ** 2) / (2 * var))
+                # log_target_probabilities.append(log_target_given_x_probability)
+                target_given_all_probabilities *= x_given_target_probability
+            # log_numerical_probabilities.append(log_target_probabilities)
+            target_probabilities.append(target_given_all_probabilities)
         
-        return log_numerical_probabilities
+        prob_sum = np.sum(target_probabilities)
+        target_probabilities = [target_probability / prob_sum for target_probability in target_probabilities]
+
+        return target_probabilities
 
     def CategoricalProbability(self):
         total_count = len(self.y_train)
