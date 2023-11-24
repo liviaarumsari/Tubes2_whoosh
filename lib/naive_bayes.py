56de4aebb88116c1ccf5e38985d287ed3ccf8534
chaerla <chelgchen@gmail.com>
2023-11-24 22:52:45 +0700

    fix: merge conflict

---
+++ lib/naive_bayes.py
@@ -0,0 +1,23 @@
+class NaiveBayesClassifier():
+    def __init__(self, alpha):
+        self.numerical_columns = []
+        self.categorical_columns = []
+        self.x_train = []
+        self.y_train = []
+        self.alpha = alpha
+
+    def fit(self, x_train, y_train):
+        pass
+
+    def predict(self, x_test):
+        pass
+
+    def GaussianProbability(self, x_test):
+        pass
+
+    def CategoricalProbability(self, x_test):
+        pass
+
+
+if __name__ == "__main__":
+    pass
